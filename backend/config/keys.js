1c3bcc7129a63483dae59ad6282271e544117a54
Saketh Reddy <sakethreddy.333@gmail.com>
2021-04-12 22:09:08 -0700

    backend setup

--- backend/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOURI: process.env.PROD ? `mongodb://${process.env.DB_HOST}:27017` : "mongodb+srv://sa1:Abcd1234@cluster0.e82da.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET: "abcdefg"
-}
