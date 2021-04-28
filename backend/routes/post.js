5001e944f3697c120631a66cfa0041a85785bcb9
Sujatha-N <79035205+Sujatha-N@users.noreply.github.com>
2021-04-28 15:31:19 -0700

    Update post.js

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -474,3 +474,200 @@ router.post("/api/updateReturnedResource", async (req, res) => {
 });
 
 //line 493 done ---------
+
+router.post('/api/addInward', (req,res)=>{
+        const {rows,  suppliedBy, sourcedBy, organization, comments, date, billNo} = req.body.values
+        rows.map(row => {
+            const resource = row.resource;
+            const price = row.price;
+            const quantity = row.quantity;
+            const GST   = row.GST;
+            const inward = new InwardOutward({
+                resource: resource,
+                price: price,
+                quantity: quantity,
+                supplier: suppliedBy,
+                sourced_by : sourcedBy,
+                comments : comments,
+                organization: organization,
+                date : date,
+                billNumber : billNo,
+                GST : GST,
+                type: "Inward"
+            })
+    
+        var present_quantity=0;
+        var present_purchased_quantity=0
+        
+        Resources.findOne({identifier : resource}) 
+        .then(currentresource=>{
+            if(currentresource == null){
+                return res.json({"message" :  "cannot find the resource, please try later" + currentresource});
+            }else{
+                present_quantity = currentresource.available_quantity;
+                present_purchased_quantity = currentresource.purchased_quantity
+                used_quantity = present_purchased_quantity-present_quantity
+            }  
+        })
+        .then (async x => {
+            const promise1 = await updateResource1();
+            const promise2 = await updateResource2();
+            constPromise3 = await updateResource3();
+            const promise3 = await saveInward();
+            return promise3;
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    
+        const updateResource1 = () => {return Resources.findOneAndUpdate({identifier : resource, available_quantity : present_quantity},{$inc:{available_quantity: quantity}}, {new: true} ,(err, doc) => {
+            if (err) {
+                return console.log("Something wrong when updating data! resource = " + resource + '\n');
+            }
+            })
+            .catch(err=>{
+                console.log("err",err)
+            })
+        }
+    
+       
+        const updateResource2 = () => {return Resources.findOneAndUpdate({identifier : resource, purchased_quantity : present_purchased_quantity},{$inc:{purchased_quantity: quantity}}, {new: true} ,(err, doc) => {
+            if (err) {
+                
+                return console.log("Something wrong when updating data! resource = " + resource + '\n');
+            }
+            })
+            .catch(err=>{
+                console.log("err",err)
+            })
+        }
+    
+        const updateResource3 = () => {return Resources.findOneAndUpdate({identifier : resource, purchased_quantity : present_purchased_quantity},{$inc:{purchased_quantity: quantity}}, {new: true} ,(err, doc) => {
+            if (err) {
+                
+                return console.log("Something wrong when updating data! resource = " + resource + '\n');
+            }
+            })
+            .catch(err=>{
+                console.log("err",err)
+            })
+        }
+        
+        const saveInward = () =>{
+            return  inward.save().then(result=>{
+            return console.log("resource = " + resource + " saved successfully")
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+       
+    })
+    return res.status(200).json({message: 'Inward: All Resources updated Successfull'})
+    })
+    
+router.post('/api/loadDefaultData',(req,res)=>{
+    const organizations = [
+        {name: "Hyderabad", website: "HYD.com", phone_number:"123456789"},
+        {name: "Nellore", website: "NLR.com", phone_number:"123456789"},
+        {name: "Bangalore", website: "BNG.com", phone_number:"123456789"},
+        {name: "Mumbai", website: "MUM.com", phone_number:"123456789"},
+        {name: "Delhi", website: "DEL.com", phone_number:"123456789"},
+    ]
+
+    const location = [
+        {name: "Hyderabad", abbr: "HYD"},
+        {name: "Nellore", abbr: "NLR"},
+        {name: 'Bangalore',abbr: 'BNG'},
+        {name: 'Mumbai', abbr: 'MUM'},
+        {name: 'Delhi', abbr: 'DEL'},
+    ]
+    const universalUnits = [
+        {name: "meters", abbr: "m"},
+        {name: "square-meters", abbr: "sq.m"},
+        {name: 'cubic-meters',abbr: 'cu.m'},
+        {name: 'centimeters', abbr: 'cm'},
+        {name: 'square-centimeters', abbr: 'sq.cm'},
+        {name: 'cubic-centimeters', abbr: 'cu.cm'},
+        {name: 'millimeters', abbr: 'mm'},
+        {name: 'square-millimeters', abbr: 'sq.mm'},
+        {name: 'cubic-millimeters', abbr: 'cu.mm'},
+        {name: 'inches', abbr: 'in'},
+        {name: 'square-inches', abbr: 'sq.in'},
+        {name: 'cubic-inches', abbr: 'cu.in'},
+        {name: 'grams', abbr: 'gm'},
+        {name: 'kilograms', abbr: 'kg'},
+        {name: 'tonnes', abbr: 'tonnes'},
+        {name: 'litres', abbr: 'l'},
+        {name: 'millilitres', abbr: 'ml'}
+    ]
+
+    const estimatedUnits = [
+        {name: "BAGS", abbr: "BAGS"},
+        {name: "LOADS", abbr: "TRIPS"},
+        {name: "CYLINDERS",abbr: "CYLINDERS"}
+    ]
+
+    const roles = [
+        {role: "SUPER ADMIN"},
+        {role: "ORG ADMIN"},
+        {role: "ENGINEER"},
+        {role: "INVERNTORY"},
+    ]
+
+    const types=[
+        {name:"TOOL"},
+        {name:"CONSUMABLE"},
+        {name:"RAW_MATERIAL"},
+        {name:"MACHINE"},
+        {name:"VEHICLE"},
+        {name:"PEOPLE"}
+    ]
+    Types.insertMany(types)
+    .then(result=>{
+        res.json({types:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
+  
+
+    Roles.insertMany(roles)
+    .then(result=>{
+        res.json({roles:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
+    Organization.insertMany(organizations)
+    .then(result=>{
+        res.json({organizations:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+  
+    
+    EstimatedUnits.insertMany(estimatedUnits)
+    .then(result=>{
+        res.json({estimatedUnits:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
+    UniversalUnits.insertMany(universalUnits)
+    .then(result=>{
+        res.json({universalUnits:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+module.exports = router
