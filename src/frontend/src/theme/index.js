8fff2bd09d3f7a4f046cfa63a6aa709ad39d799d
Gunjal Gupta <gunjal.gupta@sjsu.edu>
2021-05-07 03:35:26 -0700

    Color theme changed

--- src/frontend/src/theme/index.js
+++ src/frontend/src/theme/index.js
@@ -1,24 +1,14 @@
-import { createMuiTheme, colors } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core';
 import shadows from './shadows';
 import typography from './typography';
+import teal from '@material-ui/core/colors/teal';
+import cyan from '@material-ui/core/colors/cyan';
 
 const theme = createMuiTheme({
   palette: {
-    background: {
-      dark: '#F4F6F8',
-      default: colors.common.white,
-      paper: colors.common.white
-    },
-    primary: {
-      main: colors.indigo[500]
-    },
-    secondary: {
-      main: colors.indigo[500]
-    },
-    text: {
-      primary: colors.blueGrey[900],
-      secondary: colors.blueGrey[600]
-    }
+  
+    primary: teal,
+    secondary: cyan,
   },
   shadows,
   typography
